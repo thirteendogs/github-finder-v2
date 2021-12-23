a409eecbbb666d73209e3ee68a8c6261f7b82257
Thirteendogs <daniel.nova.girbau@gmail.com>
2021-12-23 01:15:04 +0100

    fix axios on search user

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -14,7 +14,7 @@ export const searchUsers = async (text) => {
       q: text
    })
 
-   const response = await github.get(`/search/users/${params}`)
+   const response = await github.get(`/search/users?${params}`)
 
    return response.data.items
 }
